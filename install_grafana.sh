952375be7608aa120e29c674637eb85a5bcca736
Vitor Mazuco <vitor.mazuco@gmail.com>
2020-02-12 10:21:11 -0300

    Add files via upload

---
+++ install_grafana.sh
@@ -0,0 +1,12 @@
+# install_grafana.sh
+# Script de instalação do Grafana em versão estável em Ubuntu Server 18.04
+
+curl https://packages.grafana.com/gpg.key | sudo apt-key add -
+sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
+sudo add-apt-repository "deb https://packages.grafana.com/oss/deb beta main"
+sudo apt-get update
+sudo apt-get -y install grafana
+sudo systemctl start grafana-server ; sudo systemctl enable grafana-server
+sudo ufw allow proto tcp from any to any port 3000
+
+
