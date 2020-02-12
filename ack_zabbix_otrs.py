8106313488d52440aefff7435f77e09930c3bdb2
Vitor Mazuco <vitor.mazuco@gmail.com>
2020-02-12 11:00:19 -0300

    Update ack_zabbix_otrs.py

--- ack_zabbix_otrs.py
+++ ack_zabbix_otrs.py
@@ -1,21 +1,22 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-###############################################################
-# Autor: Janssen dos Reis Lima - janssenreislima@gmail.com    #
-# Objetivo: Fazer o ack no evento automaticamente             #
-# Versao: 1.0                                                 #
-###############################################################
-from zabbix_api import ZabbixAPI
-import sys, re
+#!/usr/bin/env python3
 
-# Parametros de acesso da interface web do Zabbix 
+"""
+  Este script é executado automaticamente apos a abertura do ticket no OTRS
+  
+  Modificado em 12 de fevereiro de 2020
+  por Vitor Mazuco (vitor.mazuco@gmail.com)
+"""
+
+from zabbix_api import ZabbixAPI
+import sys
+ 
 server = "http://localhost/zabbix"
-username = "Admin"              
+username = "Admin"             
 password = "zabbix"     
  
-zapi = ZabbixAPI(server = server)
-zapi.login(username, password)
+conexao = ZabbixAPI(server = server)
+conexao.login(username, password)
 
-zapi.event.acknowledge({"eventids": sys.argv[1], "message": "Ticket " + str(sys.argv[2]) + " criado no OTRS."})
+reconhecer_evento = conexao.event.acknowledge({"eventids": sys.argv[1], "message": "Ticket " + str(sys.argv[2]) + " criado no OTRS."})
 
-
+# publicado originalmente: https://github.com/janssenlima/zabbix-otrs
