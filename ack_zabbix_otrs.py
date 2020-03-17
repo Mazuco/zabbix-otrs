0771fd6558154465998d47908e871dd5eadcc103
Vitor Mazuco <vitor.mazuco@gmail.com>
2020-03-17 19:30:36 -0300

    Update ack_zabbix_otrs.py

--- ack_zabbix_otrs.py
+++ ack_zabbix_otrs.py
@@ -19,4 +19,9 @@ conexao.login(username, password)
 
 reconhecer_evento = conexao.event.acknowledge({"eventids": sys.argv[1], "message": "Ticket " + str(sys.argv[2]) + " criado no OTRS."})
 
+### FOR ZABBIX 4.x uncomment two lines, and comment the line above
+#conn.event.acknowledge({"eventids": sys.argv[1], "action": 2, "message": "Ticket " + str(sys.argv[2]) + " create in OTRS."})
+#conn.event.acknowledge({"eventids": sys.argv[1], "action": 4, "message": "Ticket " + str(sys.argv[2]) + " create in OTRS."})
+
 # publicado originalmente: https://github.com/janssenlima/zabbix-otrs
+# e pelo https://github.com/Iakim/Zabbix-OTRS
